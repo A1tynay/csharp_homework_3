7e928b159216c6a4eb6936ce2b8fd0ac0327aa64
Altynay <altynaytileumuratova@gmail.com>
2024-11-01 15:19:11 +0500

    Add 2

---
+++ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив на 10 целых чисел. Напишите программу
+// котрая определяет количество четных чисел в массиве
+
+int[] array = {2, 13, 33, 56, 89, 96, 23, 8, 90, 76};
+
+foreach (int number in array)
+{
+    Console.Write(number + " ");
+}
+System.Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
